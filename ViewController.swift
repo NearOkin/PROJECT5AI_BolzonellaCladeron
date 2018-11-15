f53292b4467f1d1f6e526b35da7966d1bdb583bc
NearOkin <44995572+NearOkin@users.noreply.github.com>
2018-11-15 09:26:06 +0100

    ADDITION

--- ViewController.swift
+++ ViewController.swift
@@ -5,30 +5,30 @@
 //  Created by Studente on 15/11/2018.
 //  Copyright © 2018 ITT E. Barsanti. All rights reserved.
 //
-
 import UIKit
 
 class ViewController: UIViewController {
-
-  
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
     }
     //collegare la variabile chiamata myValue e il button chiamato fromStringToInt
+ 
+    @IBOutlet weak var myValue: UITextField!
+    @IBOutlet weak var myLabel: UILabel!
     @IBAction func fromStringToInt(_ sender: Any) {
-       let value = Int(myValue.text!) 
+        let value = Int(myValue.text!)
         
         if(value != nil){
-            let output = Int(value * value)
+            let output = Int(value! * value!)
             myLabel.text = "L'area è \(output)"
         }else{
-           myLabel.text = "please enter a number
+            myLabel.text = "please enter a number"
         }
         
     }
-
-
 }
 
